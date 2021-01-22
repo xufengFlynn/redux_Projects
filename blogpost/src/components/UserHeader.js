09f9b376a8a336f1cac30baba5fad068905afee8
xufengFlynn <zhangxufeng26@gmail.com>
2021-01-21 16:53:37 -0800

    Finished

---
+++ blogpost/src/components/UserHeader.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class UserHeader extends React.Component {
+  render() {
+    // const { user } = this.props;
+    const user = this.props.user;
+
+    if (!user) {
+      return null;
+    }
+
+    return <div className="header">{user.name}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps)(UserHeader);
