46101daaf39f9add194eff5026985d3f3d583df1
xufengFlynn <zhangxufeng26@gmail.com>
2021-01-25 12:39:37 -0800

    Navigate with React Router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click Me!</button>
+      <Link to="/">Navigate to Page One</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" exact component={PageTwo} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
